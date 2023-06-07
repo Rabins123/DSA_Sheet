5d7e28d5d07f3b13fb3bd8a533e1522f7b62a13d
Rabins Chaudhary <rabinschaudhary76882@gmail.com>
2023-06-07 23:42:59 +0530

    commit3

--- Heap/HeapBasic1.java
+++ Heap/HeapBasic1.java
@@ -19,11 +19,25 @@ public class HeapBasic1 {
         //printing 
         System.out.println(pq);
 
-         // peek() - Retrieves the head of the priority queue without removing it
+        // peek() - Retrieves the head of the priority queue without removing it
          int head = pq.peek();
          System.out.println("Head: " + head);  // Output: Head: 3
 
-         
+
+        // poll() - Retrieves and removes the head of the priority queue
+        int removedElement = pq.poll();
+        System.out.println("Removed Element: " + removedElement);  // Output: Removed Element: 3
+
+        // size() - Returns the number of elements in the priority queue
+        int size = pq.size();
+        System.out.println("Size: " + size);  // Output: Size: 2
+
+        // isEmpty() - Checks whether the priority queue is empty
+        boolean empty = pq.isEmpty();
+        System.out.println("Is Empty: " + empty);  // Output: Is Empty: false
+
+         // Printing the elements in the priority queue
+         System.out.println("Priority Queue: " + pq);  // Output: Priority Queue: [5, 10]
     }
 
 }
