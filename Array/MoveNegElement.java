c46901c6a9f9f6081a67f8f87c930bdc4b78b441
Rabins Chaudhary <rabinschaudhary76882@gmail.com>
2023-06-08 16:18:08 +0530

    move neg element one side using two pointer method

--- Array/MoveNegElement.java
+++ Array/MoveNegElement.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 public class MoveNegElement{
     
     //------------------------------Approach1-Using Sorting----------------------------
-    //tc-
-    //sc-
+    //tc-nlogn
+    //sc-1
     public static void moveUsingSorting(int[]nums){
         Arrays.sort(nums);
         //print
@@ -15,7 +15,40 @@ public class MoveNegElement{
         }
     }
 
+    //------------------------------Approach2-Using quick sort partitioning method ----------------------------
+    //tc-
+    //sc-
 
+    //------------------------------Approach3-two pointer method ----------------------------
+    //tc-n
+    //sc-1
+    public static void moveUsingTwoPointer(int nums3[]){
+        int start=0;
+        int end=nums3.length-1;
+        while(start<end){
+            if(nums3[start]<0 && nums3[end]<0){
+                start++;
+            }
+            else if(nums3[start]>0 && nums3[end]<0){
+                int temp=nums3[start];
+                nums3[start]=nums3[end];
+                nums3[end]=temp;
+                start++;
+                end--;
+            }
+            else if(nums3[start]<0 && nums3[end]>0){
+                start++;
+                end--;
+            }
+            else{
+                end--;
+            }   
+        }
+        //print
+        for(int i=0;i<nums3.length;i++){
+            System.out.print(nums3[i]+" ");
+        } 
+    }
 
 
 
@@ -32,6 +65,10 @@ public class MoveNegElement{
         System.out.println();
         System.out.println("----------------------Using Sorting Method-------------------------------");
         moveUsingSorting(arr);
+
+        System.out.println();
+        System.out.println("----------------------Using Two pointer Method-------------------------------");
+        moveUsingTwoPointer(arr);
 
     }
 }
