8c3ed61a57300ceee5da32a0598b5d985eb15e6c
Rabins Chaudhary <rabinschaudhary76882@gmail.com>
2023-06-09 22:54:17 +0530

    commit

---
+++ Matrix/rotateMatrixByRightAngle.java
@@ -0,0 +1,26 @@
+//rotate matrix by 90 degree clockwise
+/*
+ Input:
+1 2 3 
+4 5 6
+7 8 9  
+Output:
+7 4 1 
+8 5 2
+9 6 3
+
+ */
+
+package Matrix;
+
+import java.sql.Time;
+
+public class rotateMatrixByRightAngle {
+
+//------------------------------------//Approach1-Using traspose && Swapping--//------------------------------
+//Time Complexity – O(n*n)
+//Auxiliary Space – O(1)
+
+
+    
+}
