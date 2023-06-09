04c1b13b8aa56ddd8bbec852a213a4bba752b2ac
Rabins Chaudhary <rabinschaudhary76882@gmail.com>
2023-06-09 23:35:42 +0530

    commit

---
+++ Matrix/commonElementInAllRow.java
@@ -0,0 +1,19 @@
+package Matrix;
+
+//Common elements in all rows of a given matrix
+/*
+ Input:
+mat[4][5] = {{1, 2, 1, 4, 8},
+             {3, 7, 8, 5, 1},
+             {8, 7, 7, 3, 1},
+             {8, 1, 2, 7, 9},
+            };
+
+Output: 
+1 8 or 8 1
+8 and 1 are present in all rows.
+ */
+
+ public class commonElementInAllRow{
+    
+ }
