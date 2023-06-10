43771c67dfb88615f97fb95156b115f5492c7f41
Rabins Chaudhary <rabinschaudhary76882@gmail.com>
2023-06-10 13:15:33 +0530

    using approach2

--- Matrix/rowWithMax1.java
+++ Matrix/rowWithMax1.java
@@ -19,7 +19,7 @@ package Matrix;
 
 public class rowWithMax1 {
     
-    //--------------------------Approach1-Using for loop-----------------------------------------------------
+    //--------------------------Approach1-Using for loop(Brute Force)-----------------------------------------------------
     //tc-->0(m*n)
     //sc-->0(1)
     public static int rowWithMaxTime1(int[][] nums){
@@ -46,10 +46,37 @@ public class rowWithMax1 {
         
     }
 
-    //------------------------------Approach2----------------------------------------
+    //------------------------------Approach2-->Binary Search-------------------------------
+    //finding first index of 1 in each row
     //tc-
     //sc-
 
+     //------------------------------Approach3-->-------------------------------
+    //finding first index of 1 in each row
+    //tc-log(m+n)
+    //sc-
+
+public static int rowWithMaxTime1Approach3(int[][] nums){
+        int row=-1;            //if not exist return-1
+        int col=nums[0].length-1;
+        //traversing row by row
+        for(int i=0;i<nums.length;i++){
+           //traversing column from right to left
+            for(int j= col;j>=0;j--){
+               if(nums[i][j]==1){
+                row=i;
+                //will not see same column again
+                col--;
+               }
+               //if encounter zero 
+               else{
+                break;
+               }
+            }
+        }
+        return row;
+        
+    }
 
     //------------------------------------------------------------------------------
    // main method
@@ -59,8 +86,12 @@ public class rowWithMax1 {
            {1, 1, 1, 1},
            {0, 0, 0, 0}};
 
-           //calling the function
-           int ans=rowWithMaxTime1(arr);
-           System.out.println("Row with max1: "+ ans);
+           //calling the function1
+           int ans1=rowWithMaxTime1(arr);
+           System.out.println("Row with max1 using Approach1: "+ ans1);
+
+            //calling the function3
+           int ans3=rowWithMaxTime1(arr);
+           System.out.println("Row with max1 using Approach3: "+ ans3);
    }
 }
