476523b24879c27f58442adc19147b46ccd5e923
Rabins Chaudhary <rabinschaudhary76882@gmail.com>
2023-06-10 13:23:25 +0530

    problem defined

---
+++ Matrix/KthSmallestElementInMatrix.java
@@ -0,0 +1,21 @@
+package Matrix;
+
+//Find the kth smallest element in the matrix.--->GFG
+/*
+ Input:
+N = 4
+mat[][] =     {{10, 20, 30, 40}
+                   {15, 25, 35, 45}
+                   {24, 29, 37, 48}
+                   {32, 33, 39, 50}}
+K = 7
+Output: 30
+Explanation: 30 is the 7th smallest element.
+
+Expected Time Complexity: O(K*Log(N))
+Expected Auxiliary Space: O(N)
+ */
+
+public class KthSmallestElementInMatrix {
+    
+}
