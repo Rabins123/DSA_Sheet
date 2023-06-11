b4c1f8f3453afc55a4aab89497900eef15890479
Rabins Chaudhary <rabinschaudhary76882@gmail.com>
2023-06-11 17:53:00 +0530

    soting the row wise columnwise sorted matrix

---
+++ Matrix/SortMatrix.java
@@ -0,0 +1,36 @@
+package Matrix;
+
+//Given an NxN matrix Mat. Sort all elements of the matrix.
+/*
+ Input:
+N=4
+Mat=[[10,20,30,40],
+[15,25,35,45] 
+[27,29,37,48] 
+[32,33,39,50]]
+Output:
+10 15 20 25 
+27 29 30 32
+33 35 37 39
+40 45 48 50
+Explanation:
+Sorting the matrix gives this result.
+
+Expected Time Complexity:O(N2LogN)
+Expected Auxillary Space:O(N2)
+ */
+public class SortMatrix {
+    
+    //-------------------------------------Approach1---------------------------
+    //tc
+    //sc-
+    public int[][] sortedMatrix(int N, int Mat[][]) {
+        // code here
+    }
+
+
+
+
+    //main function
+    
+}
