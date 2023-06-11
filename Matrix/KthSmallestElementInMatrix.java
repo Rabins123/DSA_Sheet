8eb0397332b7717563480d441680c1d68e583f0e
Rabins Chaudhary <rabinschaudhary76882@gmail.com>
2023-06-11 17:16:01 +0530

    commit

--- Matrix/KthSmallestElementInMatrix.java
+++ Matrix/KthSmallestElementInMatrix.java
@@ -17,5 +17,42 @@ Expected Auxiliary Space: O(N)
  */
 
 public class KthSmallestElementInMatrix {
-    
+
+    //---------------------------approach1-Binary search-------------------
+    public static int kthSmallest(int[][]mat,int k){
+        //code here.
+        int r = mat.length;
+        int c = mat[0].length;
+        int startval=mat[0][0];
+        int endval=mat[r-1][c-1];
+        int midval;
+
+        //binary serach betwwn a range
+        while(startval<=endval){
+            midval= (startval +endval)/2;
+            //row wise traversing the matrix
+            for()
+        }
+
+        return n;
+    }
+
+
+
+
+    //main method
+    public static void main(String[] args) {
+        int arr[][]={{10, 20, 30, 40}
+                   {15, 25, 35, 45}
+                   {24, 29, 37, 48}
+                   {32, 33, 39, 50}};
+
+        int k= 7;
+
+        //calling the function
+        System.out.println("--------------------------------------------------------");
+         int ans1=kthSmallest(arr, k);
+         System.out.println("kth smallest Element is :"+ ans1);
+        System.out.println("--------------------------------------------------------");
+    }
 }
