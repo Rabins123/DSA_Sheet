e9a5c1723baa7c5b0ed6eac61aec855ecf3655c1
Rabins Chaudhary <rabinschaudhary76882@gmail.com>
2023-06-24 02:53:55 +0530

    max product sub array

---
+++ Array/maxProductSubArr.java
@@ -0,0 +1,36 @@
+package Array;
+//maxm product subarray
+
+/*
+ Input:
+N = 5
+Arr[] = {6, -3, -10, 0, 2}
+Output: 180
+Explanation: Subarray with maximum product
+is [6, -3, -10] which gives product as 180
+ */
+
+
+public class maxProductSubArr {
+  
+    //----------------------------------------------------------------------------------------------------------------//
+    //using  kadanes algorithm
+    //tc-
+    //sc-
+
+
+    //---------------------------------------------------------------------------------------------------------------------
+    //using brute force---generate all possible subarrray
+    //tc-o(n*n);
+    //sc-0(1)
+    
+    //main function
+    public static void main(String[] args) {
+       
+        int ans1[]={6, -3, -10, 0, 2};
+
+        //call the function
+        
+
+    }
+}
