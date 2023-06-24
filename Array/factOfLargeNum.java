19d3cdb95dc0e49b0c217a83916369a1faba028c
Rabins Chaudhary <rabinschaudhary76882@gmail.com>
2023-06-24 11:52:37 +0530

    factorial of large number

---
+++ Array/factOfLargeNum.java
@@ -0,0 +1,25 @@
+package Array;
+
+//factorial of large number
+
+
+public class factOfLargeNum{
+   
+//---------------------------------------------------------Using LinkedList----------------------------------------------------
+//tc-
+//sc-
+
+
+
+//---------------------------------------------------------Using ArrayList----------------------------------------------------
+//tc-
+//sc-
+    
+    //main function
+
+    public static void main(String[] args) {
+        int n=10;
+
+        //calling the function
+    }
+}
