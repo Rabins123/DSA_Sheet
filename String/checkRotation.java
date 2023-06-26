86f00129c23e405768fe285b9d87960d8ec77871
Rabins Chaudhary <rabinschaudhary76882@gmail.com>
2023-06-26 19:40:01 +0530

    using contanation

--- String/checkRotation.java
+++ String/checkRotation.java
@@ -2,4 +2,24 @@ package String;
 
 public class checkRotation {
     
+
+
+    //main function
+    public static void main(String[] args) {
+        String s1="rabins";
+        String s2="abinsr";
+
+        //call the function
+        boolean ans1= isRotationOfOther(s1, s2);
+        System.out.println("Is s2 rotation of s1 ?" + ans1);
+
+    }
+//-----------------------------Using Concatenation-------------------------------------------------------------------------------
+//tc-
+//sc-
+    public static boolean isRotationOfOther(String s1, String s2) {
+
+        String str= s1+s1;
+        return str.contains(s2);
+    }
 }
