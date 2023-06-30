ef3b5c0788ef8062a6b9f22a7e0543fc36992774
Rabins Chaudhary <rabinschaudhary76882@gmail.com>
2023-07-01 01:32:10 +0530

    commit

---
+++ Array/SuArraySum0.java
@@ -0,0 +1,43 @@
+package Array;
+
+//subarray sum 0
+
+/*
+ Input:
+5
+4 2 0 1 6
+
+Output: 
+Yes
+
+Explanation: 
+0 is one of the element 
+in the array so there exist a 
+subarray with sum 0.
+
+Expected Time Complexity: O(n).
+Expected Auxiliary Space: O(n).
+ */
+
+
+public class SuArraySum0 {
+  
+   //-------------------------------------------------------------Using Brute force------------------------------------------------
+   //tc
+   //sc 
+    public static boolean findsum1(int arr[],int n){
+
+    }
+    
+        
+    
+    
+
+    //main function
+    public static void main(String[] args) {
+        
+
+        //call the function
+
+    }
+}
