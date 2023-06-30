05da1d88dfe4bf55c58da13d885dd278d076c458
Rabins Chaudhary <rabinschaudhary76882@gmail.com>
2023-07-01 01:14:22 +0530

    common element

--- Array/CommonElement.java
+++ Array/CommonElement.java
@@ -1,6 +1,7 @@
 package Array;
 
 import java.util.ArrayList;
+import java.util.List;
 
 // find common element from three sorted arrray
 /*
@@ -18,34 +19,59 @@ Expected Auxiliary Space: O(n1 + n2 + n3)
 
 public class CommonElement {
 
-    public static ArrayList<Integer> commonElement(int[] A, int[] B, int[] C, int n1, int n2, int n3) {
-        // ArrayList<Integer> list = new ArrayList<>();
+   public static List<Integer> findCommonElements1(int[] A, int[] B, int[] C) {
+        List<Integer> commonElements = new ArrayList<>();
+
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < B.length; j++) {
+                for (int k = 0; k < C.length; k++) {
+                    if (A[i] == B[j] && B[j] == C[k]) {
+                        commonElements.add(A[i]);
+                        break;
+                    }
+                }
+            }
+        }
+
+        return commonElements;
+    }
+
+
+    //-------------------------------------------------------------------------------------------------------------
+    //tc-
+    //sc-
+        public static List<Integer> findCommonElements2(int[] A, int[] B, int[] C) {
+        List<Integer> commonElements = new ArrayList<>();
         int i = 0, j = 0, k = 0;
-        while (i < n1 && j < n2 && k < n3) {
-            if (A[i] < B[j]) {
+
+        while (i < A.length && j < B.length && k < C.length) {
+            if (A[i] == B[j] && B[j] == C[k]) {
+                commonElements.add(A[i]);
                 i++;
+                j++;
+                k++;
             } else if (A[i] < B[j]) {
+                i++;
+            } else if (B[j] < C[k]) {
                 j++;
             } else {
                 k++;
             }
         }
-        return null;
 
+        return commonElements;
     }
 
-    // main function
     public static void main(String[] args) {
-        int[] A = { 1, 5, 10, 20, 40, 80 };
-        int[] B = { 6, 7, 20, 80, 100 };
-        int[] C = { 3, 4, 15, 20, 30, 70, 80, 120 };
-        int n1=A.length;
-        int n2=B.length;
-        int n3=C.length;
+        int[] A = {1, 5, 10, 20, 40, 80};
+        int[] B = {6, 7, 20, 80, 100};
+        int[] C = {3, 4, 15, 20, 30, 70, 80, 120};
 
-    //call the function
-    ArrayList<Integer> ans1=commonElement(A, B, C,n1, n2,  n3) ;
-    System.out.println(ans1);
+        List<Integer> result1 = findCommonElements1(A, B, C);
+        System.out.println("Common Elements: " + result1);
+        System.out.println();
 
+        List<Integer> result2 = findCommonElements2(A, B, C);
+        System.out.println("Common Elements: " + result2);
     }
 }
