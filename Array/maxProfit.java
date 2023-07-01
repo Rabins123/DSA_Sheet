4331224f6f9a9af7529d8b58652b2dd1351b7204
Rabins Chaudhary <rabinschaudhary76882@gmail.com>
2023-07-02 02:47:36 +0530

    commit

---
+++ Array/maxProfit.java
@@ -0,0 +1,19 @@
+package Array;
+
+//Maximum profit by buying and selling a share at most twice
+
+/*
+Input:   price[] = {10, 22, 5, 75, 65, 80}
+Output:  87
+Trader earns 87 as sum of 12, 75 
+Buy at 10, sell at 22, 
+Buy at 5 and sell at 80
+ */
+
+
+public class maxProfit {
+  
+    
+    //mainfunction
+    
+}
