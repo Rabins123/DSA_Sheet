8364efe10c1c60426bb979ef058d3e7a35434cd9
Rabins Chaudhary <rabinschaudhary76882@gmail.com>
2023-07-02 03:37:53 +0530

    inversion count using nested loop

--- Array/inversionCount.java
+++ Array/inversionCount.java
@@ -13,10 +13,26 @@ Expected Auxiliary Space: O(N).
 
 public class inversionCount {
   
-    
-
-    //main function
-    public static void main(String[] args) {
+   public static int getInversionCount(int[] arr) {
+        int inversionCount = 0;
+        int n = arr.length;
         
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] > arr[j]) {
+                    inversionCount++;
+                }
+            }
+        }
+        
+        return inversionCount;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 1, 3, 5};
+
+        int inversionCount = getInversionCount(arr);
+
+        System.out.println("Inversion Count: " + inversionCount);
     }
 }
