c1390cf5f1995601885e38603614da5fb7507072
Rabins Chaudhary <rabinschaudhary76882@gmail.com>
2023-07-03 01:49:05 +0530

    creating node

---
+++ LINKEDLIST/Node.java
@@ -0,0 +1,14 @@
+package LINKEDLIST;
+
+//creating Node
+
+public class Node {
+    int data;
+    Node next;
+
+    //constructor
+    public Node(int x){
+        data=x;
+        next=null;
+    }
+}
