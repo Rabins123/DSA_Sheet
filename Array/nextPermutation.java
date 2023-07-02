cac78dbe7024259a8928af667cbf6220d0fe345b
Rabins Chaudhary <rabinschaudhary76882@gmail.com>
2023-07-03 01:44:28 +0530

    commit

---
+++ Array/nextPermutation.java
@@ -0,0 +1,24 @@
+package Array;
+
+//next permutation
+/*
+ dictionary order
+ Example 1:
+
+Input: nums = [1,2,3]
+Output: [1,3,2]
+Example 2:
+
+Input: nums = [3,2,1]
+Output: [1,2,3]
+Example 3:
+
+Input: nums = [1,1,5]
+Output: [1,5,1]
+
+ */
+
+ 
+public class nextPermutation {
+    
+}
